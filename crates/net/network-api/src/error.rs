69a8294e594ccf523b73fb41fb5b08fbcddc3ac7
Chris Tian <chris@succinct.xyz>
2024-02-20 14:21:36 -0800

    nuke network code

--- crates/net/network-api/src/error.rs
+++ crates/net/network-api/src/error.rs
@@ -1,5 +1,4 @@
 use thiserror::Error;
-use tokio::sync::{mpsc, oneshot};
 
 /// Network Errors
 #[derive(Error, Debug, Clone, PartialEq, Eq)]
@@ -7,16 +6,4 @@ pub enum NetworkError {
     /// Indicates that the sender has been dropped.
     #[error("sender has been dropped")]
     ChannelClosed,
-}
-
-impl<T> From<mpsc::error::SendError<T>> for NetworkError {
-    fn from(_: mpsc::error::SendError<T>) -> Self {
-        NetworkError::ChannelClosed
-    }
-}
-
-impl From<oneshot::error::RecvError> for NetworkError {
-    fn from(_: oneshot::error::RecvError) -> Self {
-        NetworkError::ChannelClosed
-    }
 }
